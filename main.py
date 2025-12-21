7e3716a0f9a23991a8b2e05e94bd475db699db6c
jack workspace <jackworksp@gmail.com>
2025-12-21 22:30:33 +0530

    fyers issue update

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from routes.profile import router as profile_router
 from routes.portfolio import router as portfolio_router
 from routes.market_data import router as market_data_router
 from routes.logs import router as logs_router
-# from routes.fyers import router as fyers_router  # Temporarily disabled - requires fyers_apiv3
+from routes.fyers import router as fyers_router
 from routes.deps import get_current_user
 
 from services.request_logger import RequestLogger
@@ -66,7 +66,7 @@ app.include_router(portfolio_router, dependencies=protected)
 app.include_router(nse_data_router, dependencies=protected)
 app.include_router(market_data_router, dependencies=protected)
 app.include_router(logs_router, dependencies=protected)
-# app.include_router(fyers_router, dependencies=protected)  # Temporarily disabled
+app.include_router(fyers_router, dependencies=protected)
 
 
 
